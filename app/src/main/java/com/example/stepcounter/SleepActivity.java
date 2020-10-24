2e4c81f816ba132a43807465608dd9f3d404d6f0
lokendra26 <lokendramalav1999@gmail.com>
2020-10-24 18:11:08 +0530

    profile activity started

--- app/src/main/java/com/example/stepcounter/SleepActivity.java
+++ app/src/main/java/com/example/stepcounter/SleepActivity.java
@@ -6,15 +6,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SleepActivity extends AppCompatActivity {
 
+    private TextView ageTextView;
+    private TextView sleepView;
+    int sleephour=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+
+        ageTextView = findViewById(R.id.ageTextView);
+        sleepView = findViewById(R.id.sleepView);
+
+
+        String ageString = ageTextView.getText().toString();
+        int value=0;
+        if (!"".equals(ageString)) {
+            value = Integer.parseInt(ageString);
+        }
+
+        if (value < 0) {
+            sleepView.setText("Age value can not be negative.");
+        }
+        else {
+            if (value < 1) {
+                    sleephour=15;
+            } else if (1 <= value && value <= 2) {
+                sleephour=13;
+            } else if (3 <= value && value <= 5) {
+                sleephour=12;
+            } else if (6 <= value && value <= 13) {
+                sleephour=11;
+            } else if (14 <= value && value <= 17) {
+                sleephour=10;
+            } else if (18 <= value && value <= 25) {
+                sleephour=9;
+            } else if (26 <= value && value <= 64) {
+                sleephour=9;
+            } else if (value >= 65) {
+                sleephour=8;
+        }
+            sleepView.setText(String.valueOf(sleephour) + " Hrs");
+    }
+
+
 
         //Initialize And Assign Variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
